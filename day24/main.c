b1f74c2aad4c5fa55779d017394067f92095f03f
Aditya Diwakar <aditya@diwakar.io>
2021-12-24 15:45:43 -0500

    :tada: Finish Day 24

---
+++ day24/main.c
@@ -0,0 +1,292 @@
+#import <stdio.h>
+#import <math.h>
+int main() {
+	for (int i0 = 5; i0 <= 9; i0++) {
+	for (int i1 = 5; i1 <= 9; i1++) {
+	for (int i2 = 5; i2 <= 9; i2++) {
+	for (int i3 = 5; i3 <= 9; i3++) {
+	for (int i4 = 5; i4 <= 9; i4++) {
+	for (int i5 = 5; i5 <= 9; i5++) {
+	for (int i6 = 5; i6 <= 9; i6++) {
+	for (int i7 = 5; i7 <= 9; i7++) {
+	for (int i8 = 5; i8 <= 9; i8++) {
+	for (int i9 = 5; i9 <= 9; i9++) {
+	for (int i10 = 5; i10 <= 9; i10++) {
+	for (int i11 = 5; i11 <= 9; i11++) {
+	for (int i12 = 5; i12 <= 9; i12++) {
+	for (int i13 = 5; i13 <= 9; i13++) {
+		int w = 0;
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		w = i0;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 11;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 3;
+		y *= x;
+		z += y;
+		w = i1;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 14;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 7;
+		y *= x;
+		z += y;
+		w = i2;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 13;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 1;
+		y *= x;
+		z += y;
+		w = i3;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += -4;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 6;
+		y *= x;
+		z += y;
+		w = i4;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 11;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 14;
+		y *= x;
+		z += y;
+		w = i5;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 10;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 7;
+		y *= x;
+		z += y;
+		w = i6;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += -4;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 9;
+		y *= x;
+		z += y;
+		w = i7;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += -12;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 9;
+		y *= x;
+		z += y;
+		w = i8;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 10;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 6;
+		y *= x;
+		z += y;
+		w = i9;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += -11;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 4;
+		y *= x;
+		z += y;
+		w = i10;
+		x *= 0;
+		x += z;
+		z = trunc(z / 1);
+		z %= 1;
+		x += 12;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 0;
+		y *= x;
+		z += y;
+		w = i11;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += -1;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 7;
+		y *= x;
+		z += y;
+		w = i12;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += 0;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 12;
+		y *= x;
+		z += y;
+		w = i13;
+		x *= 0;
+		x += z;
+		z = trunc(z / 26);
+		z %= 26;
+		x += -11;
+		x = (int)(x == w);
+		x = (int)(x == 0);
+		y *= 0;
+		y += 25;
+		y *= x;
+		y += 1;
+		z *= y;
+		y *= 0;
+		y += w;
+		y += 1;
+		y *= x;
+		z += y;
+		printf("%d\n", z);
+		if (z == 0) {
+			printf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13);
+		}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+}
